e49b75185357fdcce8663a0a7b888133a54af95e
A. Hinton <7063649+ahcode0919@users.noreply.github.com>
2019-08-12 23:29:04 -0700

    Complete telephone number algorithm

---
+++ tests/fcc-course-projects/telephone_number.test.js
@@ -0,0 +1,10 @@
+import { telephoneCheck } from "../../src/fcc-course-projects/telephone_number";
+
+test('should check telephone numbers', () => {
+  expect(telephoneCheck("2323dfsf")).toBe(false);
+  expect(telephoneCheck("5555555555")).toBe(true);
+  expect(telephoneCheck("1 555-555-5555")).toBe(true);
+  expect(telephoneCheck("555-555-5555")).toBe(true);
+  expect(telephoneCheck("1 555)555-5555")).toBe(false);
+  expect(telephoneCheck("(6054756961)")).toBe(false);
+});
