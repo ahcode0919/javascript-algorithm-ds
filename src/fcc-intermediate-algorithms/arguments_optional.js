305bf7c4272a8670ce29c093472a69cfff35174d
A. Hinton <7063649+ahcode0919@users.noreply.github.com>
2020-02-20 12:10:56 -0800

    Prettier

--- src/fcc-intermediate-algorithms/arguments_optional.js
+++ src/fcc-intermediate-algorithms/arguments_optional.js
@@ -1,14 +1,14 @@
-export function addTogether(...arr) {
-  if (!arr.every(val => typeof(val) === 'number')) {
+export default function addTogether(...arr) {
+  if (!arr.every(val => typeof val === "number")) {
     return undefined;
   }
-  if (arr.length == 2) {
+  if (arr.length === 2) {
     return arr[0] + arr[1];
   }
-  return (val) => {
-    if (typeof(val) !== 'number') {
+  return val => {
+    if (typeof val !== "number") {
       return undefined;
     }
     return val + arr[0];
-  }
+  };
 }
--- src/fcc-intermediate-algorithms/convert_html_entities.js
+++ src/fcc-intermediate-algorithms/convert_html_entities.js
@@ -1,24 +1,24 @@
 export function convertHTML(str) {
-    const getEscapedString = function(char) {
-        switch (char) {
-            case '>':
-                return '&gt;';
-            case '&':
-                return '&amp;';
-            case '<':
-                return '&lt;';
-            case '\'':
-                return '&apos;';
-            case '\"':
-                return '&quot;';
-            default:
-                return char;
-        }
+  const getEscapedString = function(char) {
+    switch (char) {
+      case ">":
+        return "&gt;";
+      case "&":
+        return "&amp;";
+      case "<":
+        return "&lt;";
+      case "'":
+        return "&apos;";
+      case '"':
+        return "&quot;";
+      default:
+        return char;
     }
-    let newString = [];
-    for (let char of str) {
-        newString.push(getEscapedString(char));
-    }
+  };
+  let newString = [];
+  for (let char of str) {
+    newString.push(getEscapedString(char));
+  }
 
-    return newString.join('');
+  return newString.join("");
 }
